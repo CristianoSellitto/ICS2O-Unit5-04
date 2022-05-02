04414671165278d42ac73d867c34e0f0043bb9a7
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-05-02 17:03:06 +0000

    Added program

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit5-04-HTML
+// May 2 2022
 
 'use strict'
 /**
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/<RESPITORY>/sw.js", {
-    scope: "/<RESPITORY>/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-04-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-04-HTML/",
   })
 }
 
 /**
- * Alerts "Hello, World!"
+ * Checks if the user is eligible for student pricing for a museum depending on their age and the current day of the week.
  */
 function onButtonClick() {
-  alert("Hello, World!")
+  const age = document.getElementById("age").value
+  const day = document.getElementById("days").value
+
+  if (day == "tuesday" || day == "thursday" || age > 12 && age < 22) {
+    document.getElementById("result").innerHTML = "<h5>You can pay with the museum's student pricing.</h5>"
+  } else {
+    document.getElementById("result").innerHTML = "<h5>You must pay with the museum's regular pricing.</h5>"
+  }
 }
